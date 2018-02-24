abfaefda7b565bdf1346b13866cbe90d2c633b57
agent-whisper <fariz.tumbuan@gmail.com>
2018-02-24 20:10:09 +0700

    bikin folder tugas peta

---
+++ tgs6_peta_itb/headers/VectorPath.h
@@ -0,0 +1,54 @@
+#ifndef VECTOR_PATH_H
+#define VECTOR_PATH_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "VectorPoint.h"
+
+typedef struct VectorPath {
+	struct VectorPoint **firstPoint;
+	struct VectorPoint **lastPoint;
+	struct VectorPoint **criticalPoints;
+	int minX;
+	int minY;
+	int maxX;
+	int maxY;
+	int numOfPoints;
+} VectorPath;
+
+// Allocates memory for a VectorPath. The lastPoint member variable will be determined by the firstPoint path.
+VectorPath* createVectorPath(struct VectorPoint* _firstPoint);
+// Creates VectorPath based on external text file
+VectorPath* createVectorPathFromFile(char* fileName);
+// Frees memory of a VectorPath along with all the points it contains
+void freeVectorPath(VectorPath* vectorPath);
+// Updates the path's min and max
+void checkForMinMaxUpdate(VectorPath* path);
+
+// Append a VectorPoint at the end of a path (becomes the new last point)
+void appendToPath(VectorPath* vectorPath, struct VectorPoint* vectorPoint);
+// Append a VectorPoint at the beginning of a path (becomes the new first point)
+void prependToPath(VectorPath* vectorPath, struct VectorPoint* vectorPoint);
+// Insert a point after a certain point in a path given it actually exists
+void insertToPath(VectorPath* vectorPath, struct VectorPoint* targetPoint, struct VectorPoint* newPoint);
+
+// Removes the first point in a path (changes the first point pointer if one exists)
+void removeFirstPoint(VectorPath* vectorPath);
+// Removes the last point in a path (changes the last point pointer if one exists)
+void removeLastPoint(VectorPath* vectorPath);
+// Removes a certain point of a path if the address provided exists in the path (the path will stay connected if possible)
+void removePoint(VectorPath* vectorPath, struct VectorPoint* targetPoint);
+
+// Enclose a path if possible
+void enclosePath(VectorPath* vectorPath);
+// Returns 1 if path is closed, 0 otherwise
+int checkIfPathIsClosed(VectorPath* vectorPath);
+// Returns 1 if specified point address is a member of the path, 0 if not found, and -1 if an error occured
+int isPathMember(VectorPath* path, struct VectorPoint* wantedPoint);
+// Return the address of point with the same coordinate specified if exist, NULL otherwise
+struct VectorPoint* findPathMemberByCoordinate(VectorPath* path, float _x, float _y);
+
+void printPoint(VectorPoint* input);
+void printPath(VectorPath* path);
+#endif
---
+++ tgs6_peta_itb/headers/VectorPoint.c
@@ -0,0 +1,76 @@
+#include "VectorPoint.h"
+
+VectorPoint* createVectorPoint(double _x, double _y) {
+	VectorPoint* newPoint = malloc(sizeof(VectorPoint));
+
+	if (newPoint == NULL) {
+		printf("[VectorPoint.c => createVectorPoint()] Failed to allocate memory for VectorPoint");
+		return NULL;
+	}
+
+	newPoint->x = _x;
+	newPoint->y = _y;
+	newPoint->nextPoint = malloc(sizeof(VectorPoint*));
+	if (newPoint->nextPoint == NULL) {
+		printf("[VectorPoint.c => createVectorPoint()] Failed to allocate memory for nextPoint pointer");
+		free(newPoint);
+		return NULL;
+	}
+	newPoint->isCrit == 0;
+	newPoint->prevPoint = malloc(sizeof(VectorPoint*));
+	if (newPoint->prevPoint == NULL) {
+		printf("[VectorPoint.c => createVectorPoint()] Failed to allocate memory for prevPoint pointer");
+		free(newPoint->nextPoint);
+		free(newPoint);
+		return NULL;
+	}
+
+	newPoint->nextPoint[0] = NULL;
+	newPoint->prevPoint[0] = NULL;
+
+	return newPoint;
+}
+
+void freeVectorPoint(VectorPoint* vectorPoint) {
+	if (vectorPoint != NULL) {
+		if (vectorPoint->prevPoint[0] != NULL) {
+			vectorPoint->prevPoint[0]->nextPoint[0] = NULL;
+			free(vectorPoint->prevPoint);
+		}
+
+		if (vectorPoint->nextPoint[0] != NULL) {
+			vectorPoint->nextPoint[0]->prevPoint[0] = NULL;
+			free(vectorPoint->nextPoint);
+		}
+		free(vectorPoint);
+
+		printf("successfully freed point\n");
+	}
+}
+
+void setNextPoint(VectorPoint* currentPoint, VectorPoint* _nextPoint) {
+	if (currentPoint == NULL) {
+		printf("[VectorPoint.c => setNextPoint()] The current point has a value of NULL\n");
+		return;
+	} else if (_nextPoint == NULL) {
+		printf("[VectorPoint.c => setNextPoint()] The next point has a value of NULL\n");
+		return;
+	}
+
+	currentPoint->nextPoint[0] = _nextPoint;
+	_nextPoint->prevPoint[0] = currentPoint;
+}
+
+void setPrevPoint(VectorPoint* currentPoint, VectorPoint* _prevPoint) {
+	if (currentPoint == NULL) {
+		printf("[VectorPoint.c => setPrevPoint()] The current point has a value of NULL");
+		return;
+	} else if (_prevPoint == NULL) {
+		printf("[VectorPoint.c => setPrevPoint()] The previous point has a value of NULL");
+		return;
+	}
+
+	currentPoint->prevPoint[0] = _prevPoint;
+	_prevPoint->nextPoint[0] = currentPoint;
+
+}
